d84d53c82a37b573a98cc2230441d7b860465c4e
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 19:42:41 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,4 +6,10 @@ class Anagram
         @word = word
     end
 
+    def match(array_of_words)
+        array_of_words.collect do |element|
+            @word.split("").sort == element.split("").sort
+        end
+    end
+
     
